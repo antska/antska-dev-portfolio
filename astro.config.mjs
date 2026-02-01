7d750aed3645c11b4cb994ef7a22470678f09c85
antska <antonis.skandalis@theblueground.com>
2026-02-02 00:18:55 +0200

    feat: Integrate sitemap functionality and update robots.txt for sitemap indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    // sitemap(),
+    sitemap({
+      filter: (page) =>
+        page !== 'https://antska.dev/404' &&
+        !page.endsWith('/404/'),
+    }),
   ],
   output: 'static',
-  site: 'https://antska.dev', // Update with your actual domain
+  site: 'https://antska.dev',
 });
