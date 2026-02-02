5bba467f9d545dadfef1c6e0ed1105cb33adc79d
antska <antonis.skandalis@theblueground.com>
2026-02-02 14:03:54 +0200

    feat: Enable prefetching for improved navigation performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  prefetch: true,
   integrations: [
     tailwind(),
     sitemap({
